0b3f9ee9df3cdbe816c9a0ff9d166159dd508088
ERIN SPRINGER <erin.springer9@gmail.com>
2021-07-01 15:06:01 -0700

    added javascript and prompts

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-let userName = prompt("Enter Your Name Now");
-document.write('Welcome ' + userName + '!!')
+
+function getUserName(){
+    let userName = prompt('What is your name?');
+    document.write('<h2>' + 'welcome ' + userName + '!</h2>')
+    return userName;
+}
+
+//function 2
+function likeHike(){
+    let likeHike = prompt('do you like to hike?')
+    if (likeHike == 'yes'){
+        document.write('<h3>' + 'cool, so do we!'+ '</h3>')
+    } else {
+        document.write('<h3>' + 'Bummer');
+        }
+}
+
+
+//funciton 3
+function getTime(){
+    let today = new Date();
+    let hourNow = today.getHours();
+    let greeting;
+    if (hourNow > 18) {
+        greeting = 'Good evening';
+    } else if (hourNow > 11) {
+        greeting = 'Good afternoon';
+    } else if (hourNow >= 0) {
+        greeting = 'Good morning';
+    } else {
+        greeting = 'Whatsup';
+    }
+    document.write('<h3>' + greeting.toLowerCase() + '</h3>');
+}
+
+
+
+
 
 
--- app2.js
+++
@@ -1,7 +0,0 @@
-let likeHike = prompt("do you like to hike?")
-
-if(likeHike == 'yes'){
-    document.write('Cool, so do we!')
-} else {
-    document.write('Bummer :(')
-    }
