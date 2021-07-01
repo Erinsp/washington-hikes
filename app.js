87f31de2ef9b0fe0a4d77a85908d126c8725fd3c
ERIN SPRINGER <erin.springer9@gmail.com>
2021-07-01 02:17:01 -0700

    added css & js pages

---
+++ app.js
@@ -0,0 +1,4 @@
+let userName = prompt("Enter Your Name Now");
+document.write('Welcome ' + userName + '!!')
+
+
---
+++ app2.js
@@ -0,0 +1,7 @@
+let likeHike = prompt("do you like to hike?")
+
+if(likeHike == 'yes'){
+    document.write('Cool, so do we!')
+} else {
+    document.write('Bummer :(')
+    }
